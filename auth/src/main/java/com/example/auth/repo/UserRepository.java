e8b133c3bc878708669b21eafaff8483adb7de7b
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-01-31 12:12:11 +0100

    nie do konca jestem pewien czy to  zadziala ale narazie chyba dziala

--- auth/src/main/java/com/example/auth/repo/UserRepository.java
+++ auth/src/main/java/com/example/auth/repo/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.auth.repo;
 import com.example.auth.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +11,11 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByLogin(String login);
+    @Query(nativeQuery = true, value = "SELECT * FROM users where login=?1 and islock=false and isenabled=true")
+    Optional<User> findUserByLoginAndLockAndEnabled(String login);
+    //@Query(nativeQuery = true, value = "SELECT * FROM users where login=?1 and islock=false and isenabled=true and role='ADMIN'")
+    //Optional<User> findUserByLoginAndLockAndEnabledAndIsAdmin(String login);
     Optional<User> findUserByEmail(String email);
+    //Optional<User> findUserByUuid(String uuid);
+
 }
