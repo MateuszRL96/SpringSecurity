3992ec6204257ac7fff9485bceb57b214ff4d594
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2024-02-25 16:01:05 +0100

    poprawiony formularz logowania, dzizala dostep do profilu admina

--- auth/src/main/java/com/example/auth/fasada/AuthController.java
+++ auth/src/main/java/com/example/auth/fasada/AuthController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Slf4j
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     private final UserService userService;
 
